b4be6a3368818603584739cc5a324b51a3e5670a
Johannes Wünsche <johannes.wuensche97@gmail.com>
2020-01-03 16:49:45 +0100

    Change example canteen
    The example canteen is since a few weeks not available in the api, the example just uses the Canteen in Hannover as demonstration.

--- examples/days.rs
+++ examples/days.rs
@@ -3,8 +3,19 @@ use openmensa_rs::{req_canteens, request::DayRequest};
 #[tokio::main]
 async fn main() {
     let list = req_canteens().await.unwrap();
-    println!("Opening days from {}", list[0].name());
-    let days = DayRequest::new(list[0].id())
+    let canteen = list
+        .iter()
+        .filter_map(|elem| {
+            if elem.name().contains("Hannover") {
+                Some((elem.name(), elem.id()))
+            } else {
+                None
+            }
+        })
+        .nth(0)
+        .unwrap();
+    println!("Opening days from {}", canteen.0);
+    let days = DayRequest::new(canteen.1)
         .with_start_date(chrono::Utc::today())
         .build()
         .await;
--- examples/meals.rs
+++ examples/meals.rs
@@ -3,12 +3,23 @@ use openmensa_rs::{req_canteens, request::MealRequest};
 #[tokio::main]
 async fn main() {
     let list = req_canteens().await.unwrap();
+    let canteen = list
+        .iter()
+        .filter_map(|elem| {
+            if elem.name().contains("Hannover") {
+                Some((elem.name(), elem.id()))
+            } else {
+                None
+            }
+        })
+        .nth(0)
+        .unwrap();
     println!(
         "Meals in the {} canteen on the {}",
-        list[0].name(),
+        canteen.0,
         chrono::Utc::today()
     );
-    let meals = MealRequest::new(list[0].id(), chrono::Utc::today())
+    let meals = MealRequest::new(canteen.1, chrono::Utc::today())
         .build()
         .await
         .unwrap();
